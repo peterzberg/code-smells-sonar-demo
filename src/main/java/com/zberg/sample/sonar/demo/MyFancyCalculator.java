5421e7d4aa87c5ec79b7c13f01460cc3c2de59aa
Peter Zberg <peter.zberg@gmail.com>
2023-03-11 12:43:29 +0100

    new feature added

--- src/main/java/com/zberg/sample/sonar/demo/MyFancyCalculator.java
+++ src/main/java/com/zberg/sample/sonar/demo/MyFancyCalculator.java
@@ -3,17 +3,23 @@ package com.zberg.sample.sonar.demo;
 public class MyFancyCalculator {
 
     private final String calculatorName;
+    private String missingString;
 
     public MyFancyCalculator(String name) {
         this.calculatorName = name;
     }
 
-    public String getCalculatorName() {
+    public String getCalculatorName()
+    {
         return calculatorName;
     }
 
     public int add(int summand1, int summand2){
         return summand1 + summand2;
+    }
+
+    private int multiply(int something, int somethingElse){
+        return something * something;
     }
 
 }
